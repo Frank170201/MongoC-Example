635cd4a9edc982270c59f6fe4abb012befb03f26
Francisco Santamaria <sm19035@ues.edu.sv>
2025-02-28 16:06:42 -0600

    Agregando cambios a BaseService

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.utility;
+using static Mango.Web.utility.SD;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+
+        public object Data { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+//namespace Mango.Web.Service
+//{
+//    public class BaseService : IBaseService
+//    {
+//        private readonly IHttpClientFactory _httpClientFactory;
+//        public BaseService(IHttpClientFactory httpClientFactory)
+//        {
+//            _httpClientFactory = httpClientFactory;
+//        }
+//        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+//        {
+//            HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+//            HttpRequestMessage message = new();
+//            message.Headers.Add("Accept", "application/json");
+//            message.RequestUri = new Uri(requestDto.Url);
+//            if(requestDto.Data != null)
+//            {
+//                message.Content= new StringContent(JsonConvert)
+//            }
+//        }
+//    }
+//}
